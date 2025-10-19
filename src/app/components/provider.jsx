e071ca1091ff7145390208ad22e8452dca483c01
Ritesh Singh <j4260381@gmail.com>
2025-10-19 10:27:15 +0530

    BUgs

--- src/app/components/provider.jsx
+++ src/app/components/provider.jsx
@@ -1,7 +1,8 @@
 "use client"
 import React, { Suspense } from 'react';
 import { ConvexProvider, ConvexReactClient } from "convex/react";
-import AuthProvider from './authProvider';
+import AuthProvider from './AuthProvider';
+
 const Provider = ({ children }) => {
     const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
     return (
